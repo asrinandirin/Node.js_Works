45c18e5a8b62dcac20af1fe3da959e3106570810
asrinandirin <asrnandrn@gmail.com>
2023-02-01 15:04:47 +0300

    Started Clean Blog Project Part 1

---
+++ CleanBlog/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req,res) => {
+    const blog = {
+        id: 1,
+        title: "Blog Title",
+        description: "Blog Description"
+    }
+    res.send(blog);
+})
+
+app.listen(port, () => {
+    console.log("Server is listening ...");
+})
+
+
