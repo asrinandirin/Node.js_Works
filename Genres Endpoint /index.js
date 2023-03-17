245364e8c59bd53c4434763fb52fbd1d2364440d
asrin andirin <asrnandrn@gmail.com>
2023-03-18 02:38:28 +0300

    database integrated

--- Genres Endpoint /index.js
+++ Genres Endpoint /index.js
@@ -1,14 +1,14 @@
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
-const genres = require('./router/genres')
 
-//MIDDLEWARES
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
 app.use(express.json());
+app.use('/api/genres', genres);
 
-app.use('/api/genres',genres)
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`App is listening on port:${PORT}`);
-});
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
